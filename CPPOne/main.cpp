c76ccdc4b91b705011747bd8bc2c2b1dff578e4f
Paul Wilson <paulanthonywilson@gmail.com>
2011-07-28 22:05:01 +0100

    Initial Commit

---
+++ CPPOne/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPPOne
+//
+//  Created by Paul Wilson on 28/07/2011.
+//  Copyright 2011 EdgeCase UK Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
